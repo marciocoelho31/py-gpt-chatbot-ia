0ff1c846ed4beebe18db213aa44ffd6554f35614
Marcio Coelho Socorro <m.coelho.socorro@avanade.com>
2024-04-22 16:24:37 -0300

    Implementando functions calling

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, Response
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
-
 from time import sleep
 from helpers import *
 from selecionar_documento import *
@@ -12,13 +11,18 @@ from assistente_ecomart import *
 load_dotenv()
 
 cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-modelo = "gpt-4"
+modelo = "gpt-4-1106-preview"
 
 app = Flask(__name__)
 app.secret_key = 'marciocoelho'
 
-assistente = criar_assistente()
-thread = criar_thread()
+assistente = pegar_json()
+thread_id = assistente["thread_id"]
+assistente_id = assistente["assistant_id"]
+file_ids = assistente["file_ids"]
+
+STATUS_COMPLETED = "completed"
+STATUS_REQUIRES_ACTION = "requires_action"
 
 def bot(prompt):
     maximo_tentativas = 1
@@ -26,24 +30,63 @@ def bot(prompt):
 
     while True:
         try:
+            personalidade = personas[selecionar_persona(prompt)]
             cliente.beta.threads.messages.create(
-                 thread_id = thread.id,
+                 thread_id=thread_id,
+                 role="user",
+                 content = f"""
+                    Assuma, de agora em diante, a personalidade abaixo.
+                    Ignore as personalidades anteriores.
+
+                    # Persona
+                    {personalidade}
+                 """,
+                 file_ids = file_ids
+            )
+
+            cliente.beta.threads.messages.create(
+                 thread_id = thread_id,
                  role = "user",
-                 content = prompt
+                 content = prompt,
+                 file_ids = file_ids
             )
 
             run = cliente.beta.threads.runs.create(
-                 thread_id=thread.id,
-                 assistant_id=assistente.id
+                 thread_id=thread_id,
+                 assistant_id=assistente_id
             )
 
-            while run.status != "completed":
+            while run.status != STATUS_COMPLETED:
                  run = cliente.beta.threads.runs.retrieve(
-                      thread_id=thread.id,
+                      thread_id=thread_id,
                       run_id=run.id
                  )
+
+                 print(f"Status: {run.status}")
+
+                 if run.status == STATUS_REQUIRES_ACTION:
+                    tools_acionadas = run.required_action.submit_tool_outputs.tool_calls
+                    respostas_tools_acionadas = [] 
+
+                    for uma_tool in tools_acionadas:
+                         nome_funcao = uma_tool.function.name
+                         funcao_escolhida = minhas_funcoes[nome_funcao]
+                         argumentos = json.loads(uma_tool.function.arguments)
+                         print(argumentos)
+                         resposta_funcao = funcao_escolhida(argumentos)
+
+                         respostas_tools_acionadas.append({
+                              "tool_call_id": uma_tool.id,
+                              "output": resposta_funcao
+                         })
+
+                    run = cliente.beta.threads.runs.submit_tool_outputs(
+                         thread_id = thread_id,
+                         run_id = run.id,
+                         tool_outputs=respostas_tools_acionadas
+                    )
             
-            historico = list(cliente.beta.threads.messages.list(thread_id=thread.id).data)
+            historico = list(cliente.beta.threads.messages.list(thread_id=thread_id).data)
             resposta = historico[0]
             return resposta
         
